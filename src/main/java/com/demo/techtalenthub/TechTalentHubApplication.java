dfc14a05c47175bef4e90937c2d3ad7d1b7df54a
heisenberg2202 <ayushunplugged22@gmail.com>
2024-07-22 00:03:20 +0530

    Initiall Commit

---
+++ src/main/java/com/demo/techtalenthub/TechTalentHubApplication.java
@@ -0,0 +1,13 @@
+package com.demo.techtalenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentHubApplication.class, args);
+	}
+
+}
